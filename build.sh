c84874a649bec499ed50d02a73d2712493ecfdc1
a22nicolasmr <a22nicolasmr@iessanclemente.net>
2025-04-01 14:07:35 +0200

    cambios rutas

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # Exit on error
 set -o errexit
 
-export PYTHONPATH=$PYTHONPATH:/opt/render/project/src
+export PYTHONPATH=$PYTHONPATH:/opt/render/project/src/to_exercise_project
 
 # Install dependencies
 pip install -r requirements.txt
@@ -14,5 +14,5 @@ python to_exercise_project/manage.py migrate
 python to_exercise_project/manage.py collectstatic --no-input
 
 # Start the Django development server on the correct host and port
-gunicorn to_exercise_project.to_exercise_project.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn to_exercise_project.wsgi:application --bind 0.0.0.0:$PORT
 
