ad17dde6281ec2513c02995c0bcef359f28d57a0
a22nicolasmr <a22nicolasmr@iessanclemente.net>
2025-04-07 08:36:27 +0200

    cambiado build.sh

--- to_exercise_project/to_exercise_project/wsgi.py
+++ to_exercise_project/to_exercise_project/wsgi.py
@@ -2,6 +2,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # Aquí debe ser 'to_exercise_project.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'to_exercise_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 application = get_wsgi_application()
