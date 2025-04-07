1ff239929f51615ac3438c0c9e1c540a23ff1a82
a22nicolasmr <a22nicolasmr@iessanclemente.net>
2025-04-07 08:51:36 +0200

    cambiadas rutas

--- to_exercise_project/to_exercise_project/settings.py
+++ to_exercise_project/to_exercise_project/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'toExercise'
+    'to_exercise_project.toExercise'
 ]
 
 MIDDLEWARE = [
